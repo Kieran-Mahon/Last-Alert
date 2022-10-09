1e53846ecd5c2a9dd591edc0cbd27774757676ed
Kieran-Mahon <84115639+Kieran-Mahon@users.noreply.github.com>
2022-10-10 00:51:32 +1300

    Started on the panel class

---
+++ Last Alert/Assets/Scripts/Panel/KeyPadPanel.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPadPanel : Panel {
+
+    public override void ResetResult() {
+        completed = false;
+    }
+}
---
+++ Last Alert/Assets/Scripts/Panel/Panel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Panel : Result {
+
+    //The state of the panel
+    public State panelState = State.NOTCOMPLETED;
+
+    [Header("UI")]
+    public GameObject hintUI;
+    public GameObject uncompletedUI;
+    public GameObject completedUI;
+    
+    //Used with UI
+    public void changeState(State newState) {
+        panelState = newState;
+    }
+
+    //The state the panel is in
+    public enum State {
+        HINT,
+        NOTCOMPLETED,
+        COMPLETED
+    }
+}
