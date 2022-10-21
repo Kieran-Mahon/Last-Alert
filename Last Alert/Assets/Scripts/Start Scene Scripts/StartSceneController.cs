147d8a25a48e3a5383c0a7fabe76fd3407d1081c
Kieran-Mahon <84115639+Kieran-Mahon@users.noreply.github.com>
2022-10-21 16:50:30 +1300

    Removed start scene timer example

--- Last Alert/Assets/Scripts/Start Scene Scripts/StartSceneController.cs
+++ Last Alert/Assets/Scripts/Start Scene Scripts/StartSceneController.cs
@@ -72,7 +72,6 @@ public class StartSceneController : MonoBehaviour {
         AudioManager.instance.Play("gameBackground");
         SceneController.SwitchToGameScene();
         //SceneController.SwitchToTutorialScene();
-        GameTimer.SetTimer(100);
     }
 
     //Continue Button
