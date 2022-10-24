8332e078bceb676df66ee9c0229267ba2f8121d9
Toby-McIsaac <83680218+Toby-McIsaac@users.noreply.github.com>
2022-10-24 21:30:25 +1300

    Snapping complete

--- Last Alert/Assets/Scripts/Game Scene Scripts/Conditions/ItemSnap.cs
+++ Last Alert/Assets/Scripts/Game Scene Scripts/Conditions/ItemSnap.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class ItemSnap : MonoBehaviour
 {
+    public GameObject wantedObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject == wantedObject)
+        {
+            snapToCenter(other.gameObject);
+        }
+    }
+
+    public void snapToCenter(GameObject snapObject)
+    {
+        if (snapObject == null)
+        {
+            return;
+        }
+
+        //Stop physics
+        Rigidbody rigidbody = snapObject.GetComponent<Rigidbody>();
+        if (rigidbody != null)
+        {
+            rigidbody.isKinematic = true;
+        }
+
+        // Change position and rotation
+        snapObject.transform.position = transform.position;
+        snapObject.transform.rotation = transform.rotation;
     }
 }
